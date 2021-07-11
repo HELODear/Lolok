f7b17d232652996b4674f45337650a186319a62a
Investorer <85453613+Investorer@users.noreply.github.com>
2021-07-11 20:43:16 +0530

    Update youtube.py

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -1,6 +1,6 @@
 from os import path
 
-from youtube_dl import YoutubeDL
+from downloaders import youtube
 
 from config import BOT_NAME as bn, DURATION_LIMIT
 from helpers.errors import DurationLimitError
